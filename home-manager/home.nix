d27c697b00d7376858e17065a6578021417a1217
Tristan Gosselin-Hane <starcraft66@gmail.com>
2020-06-17 16:07:11 -0400

    Add tmux config to home-manager

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -3,6 +3,7 @@
 {
   imports = [
       ./programs/alacritty.nix
+      ./programs/tmux.nix
   ];
 
   programs.home-manager.enable = true;
---
+++ home-manager/programs/tmux.nix
@@ -0,0 +1,39 @@
+{ config, lib, pkgs, ... }:
+
+{
+  programs.tmux = {
+    sensibleOnTop = false;
+    enable = true;
+    clock24 = true;
+    historyLimit = 999999999;
+    newSession = true;
+    shortcut = "a";
+
+    extraConfig = ''
+      # Binds to split the panes
+      bind | split-window -h
+      bind - split-window -v
+      unbind '"'
+      unbind %
+
+      # Reload the running-config
+      bind r source-file ~/.tmux.conf
+
+      # gimmi dat history
+      set-option -g history-limit 999999999
+
+      # Nice Yellow Status Line
+      set -g status-style bg=yellow,fg=black
+
+      # vi mouse tweaks
+      set -g mouse on
+      bind -T copy-mode-vi M-Up              send-keys -X scroll-up
+      bind -T copy-mode-vi M-Down            send-keys -X scroll-down
+      bind -T copy-mode-vi M-PageUp          send-keys -X halfpage-up
+      bind -T copy-mode-vi M-PageDown        send-keys -X halfpage-down
+      bind -T copy-mode-vi PageDown          send-keys -X page-down
+      bind -T copy-mode-vi PageUp            send-keys -X page-up
+    '';
+  };
+}
+
