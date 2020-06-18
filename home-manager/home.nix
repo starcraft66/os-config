9c215c92f5a9182d88531984bba02fcbcad5b5e5
Tristan Gosselin-Hane <starcraft66@gmail.com>
2020-06-17 22:02:39 -0400

    Added nix-doom-emacs to home-manager configuration

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -3,6 +3,7 @@
 {
   imports = [
       ./programs/alacritty.nix
+      ./programs/doom-emacs.nix
       ./programs/tmux.nix
   ];
 
--- nix-darwin/darwin-configuration.nix
+++ nix-darwin/darwin-configuration.nix
@@ -5,10 +5,7 @@
   # $ nix-env -qaP | grep wget
   environment.systemPackages = [
     pkgs.vim
-    pkgs.emacs
   ];
-
-  services.emacs.enable = true;
 
   fonts = {
     enableFontDir = true;
