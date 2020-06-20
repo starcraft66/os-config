acdaaff4c6ccb600daab9d13d3fbf2242018a22e
Tristan Gosselin-Hane <starcraft66@gmail.com>
2020-06-19 22:42:23 -0400

    Add luna nixos config

---
+++ nixos/luna/configuration.nix
@@ -0,0 +1,170 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use the latest linux kernel
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "luna"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Garbage Collection
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.vlans.Management = { id = 52; interface = "enp3s0"; };
+  networking.vlans.Home = { id = 51; interface = "enp3s0"; };
+  networking.interfaces.enp3s0.useDHCP = false;
+  networking.interfaces.Home.useDHCP = true;
+  networking.interfaces.Management.useDHCP = true;
+  networking.wireguard.enable = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n = {
+    # consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  console.font = "Lat2-Terminus16";
+  console.useXkbConfig = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Toronto";
+
+  # (((steam))) and (((nvidia)))
+  nixpkgs.config.allowUnfree = true;
+  hardware.opengl.driSupport32Bit = true;
+  hardware.pulseaudio.support32Bit = true;
+  hardware.opengl.extraPackages = with pkgs; [ libva ];
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+
+  environment.systemPackages = with pkgs;
+  let
+    my-python-packages = python-packages: with python-packages; [
+      pip
+      pipenv
+      setuptools
+      virtualenv
+      ansible
+    ];
+    python-with-my-packages = python3.withPackages my-python-packages;
+  in [
+    wget vim wireshark networkmanager riot-desktop git pass slack qtpass pciutils
+    alacritty steam tmux neofetch spotify vscode minecraft roboto font-awesome
+    screen unzip traceroute signal-desktop iperf ethtool irssi qogir-theme libsForQt5.qtstyleplugins
+    spectacle (firefox.override { extraNativeMessagingHosts = [ passff-host ]; })
+    python-with-my-packages virtmanager-qt borgbackup thunderbird speedtest-cli
+    chromium vagrant unrar file binutils patchelf fuse zlib appimage-run net_snmp
+    tcpdump gns3-gui minecraft wireguard dislocker obs-studio htop lm_sensors
+    docker-compose bind wineWowPackages.staging winetricks
+  ];
+
+  # Virtualization
+  virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd.qemuRunAsRoot = false;
+  virtualisation.libvirtd.onBoot = "ignore";
+
+  # docker
+  virtualisation.docker.enable = true;
+  virtualisation.docker.extraOptions = "--bip 10.99.0.1/24";
+
+  # nfs shared folders
+  services.nfs.server.enable = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  programs.gnupg.agent = { enable = true; enableSSHSupport = true; enableExtraSocket = true; };
+  programs.wireshark = { enable = true; package = pkgs.wireshark; };
+
+  # List services that you want to enable:
+  location.provider = "geoclue2";
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+  };
+  services.tftpd.enable = true;
+  services.pcscd.enable = true;
+  services.fwupd.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  # kde has a "redshift" built-in
+  #services.redshift = {
+  #  enable = true;
+  #  temperature.day = 6500;
+  #  temperature.night = 2700;
+  #};
+
+  # Open ports in the firewall.
+  networking.firewall.allowedTCPPorts = [ 22 2049 111 ];
+  networking.firewall.allowedUDPPorts = [ 2049 111 ];
+  networking.firewall.logRefusedConnections = true;
+  networking.firewall.logRefusedUnicastsOnly = true;
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # u2f
+  hardware.u2f.enable = true;
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+  services.printing.drivers = [ pkgs.brlaser ];
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable touchpad support.
+  # services.xserver.libinput.enable = true;
+
+  # Enable the KDE Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.tristan = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "video" "wireshark" "libvirtd" "docker" ]; # Enable ‘sudo’ for the user.
+  };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "19.09"; # Did you read the comment?
+
+}
+
