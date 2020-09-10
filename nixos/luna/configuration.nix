87b773f12668c32cb086c7543d85a984052d799d
Tristan Gosselin-Hane <starcraft66@gmail.com>
2020-09-10 12:36:58 -0400

    Use apcupsd on Luna

--- nixos/luna/configuration.nix
+++ nixos/luna/configuration.nix
@@ -127,6 +127,9 @@ in
   # nfs shared folders
   services.nfs.server.enable = true;
 
+  # apcupsd battery shit
+  services.apcupsd.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
