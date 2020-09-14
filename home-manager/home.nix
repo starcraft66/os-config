d14ffe063ca8f26b5bb6f2ed186a078f3e40a273
Tristan Gosselin-Hane <starcraft66@gmail.com>
2020-09-13 21:53:08 -0400

    Fix formatting

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -2,17 +2,17 @@
 
 {
   imports = [
-      ./programs/alacritty.nix
-      ./programs/direnv.nix
-      ./programs/doom-emacs.nix
-      ./programs/tmux.nix
-      ./programs/fzf.nix
-      ./programs/git.nix
-      ./programs/kde.nix
-      ./programs/neofetch.nix
-      ./programs/starship.nix
-      ./programs/zoxide.nix
-      ./programs/zsh.nix
+    ./programs/alacritty.nix
+    ./programs/direnv.nix
+    ./programs/doom-emacs.nix
+    ./programs/tmux.nix
+    ./programs/fzf.nix
+    ./programs/git.nix
+    ./programs/kde.nix
+    ./programs/neofetch.nix
+    ./programs/starship.nix
+    ./programs/zoxide.nix
+    ./programs/zsh.nix
   ];
 
   programs.home-manager.enable = true;
