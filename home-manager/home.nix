9d9b6bc9513959d7a66e29997b2ce45e142d439d
Tristan Gosselin-Hane <starcraft66@gmail.com>
2020-09-14 23:08:51 -0400

    Add VS Code config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -5,12 +5,13 @@
     ./programs/alacritty.nix
     ./programs/direnv.nix
     ./programs/doom-emacs.nix
-    ./programs/tmux.nix
     ./programs/fzf.nix
     ./programs/git.nix
     ./programs/kde.nix
     ./programs/neofetch.nix
     ./programs/starship.nix
+    ./programs/tmux.nix
+    ./programs/vscode.nix
     ./programs/zoxide.nix
     ./programs/zsh.nix
   ];
---
+++ home-manager/programs/vscode.nix
@@ -0,0 +1,38 @@
+{ config, lib, pkgs, ... }:
+
+{
+  programs.vscode = {
+    enable = true;
+
+    package = pkgs.vscode;
+
+    userSettings = {
+      python.venvFolders =  [
+        "envs"
+        ".pyenv"
+        ".direnv"
+        "/home/tristan/.local/share/virtualenvs"
+        "/Users/tristan/.local/share/virtualenvs"
+      ];
+      explorer.confirmDragAndDrop = false;
+      latex-workshop.view.pdf.viewer = "tab";
+      workbench.colorTheme = "Deepdark Material Theme | Full Black Version";
+      window.zoomLevel = 1;
+      workbench.iconTheme = "vscode-icons";
+      cSpell.enabled = false;
+      python.jediEnabled = false;
+      vsicons.dontShowNewVersionMessage = true;
+      python.languageServer = "Microsoft";
+    };
+
+    # keybindings = {
+
+    # };
+
+    # not managed for now
+    extensions = [
+
+    ];
+  };
+}
+
