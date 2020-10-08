feeaec90bec1426dd5699347187d1239a870f4f7
Tristan Gosselin-Hane <starcraft66@gmail.com>
2020-10-07 23:53:51 -0400

    Add dunst config

--- home-manager/programs/i3.nix
+++ home-manager/programs/i3.nix
@@ -2,7 +2,7 @@
 
 {
   programs.i3status = {
-	  enable = true;
+    enable = true;
     enableDefault = false;
     general = {
       output_format = "i3bar";
@@ -48,6 +48,73 @@
           format = "<span background='#81a1c1'> %time </span>";
           format_time = " %a %-d %b %H:%M";
         };
+      };
+    };
+  };
+
+  services.dunst = {
+    enable = true;
+    settings = {
+      global = {
+        font = "MesloLGS Nerd Font Mono 10";
+        markup = "yes";
+        plain_text = "no";
+        format = "<b>%s</b>\n%b";
+        sort = "no";
+        indicate_hidden = "yes";
+        alignment = "center";
+        bounce_freq = 0;
+        show_age_threshold = -1;
+        word_wrap = "yes";
+        ignore_newline = "no";
+        stack_duplicates = "yes";
+        hide_duplicate_count = "yes";
+        geometry = "300x50-15+49";
+        shrink = "no";
+        transparency = 5;
+        idle_threshold = 0;
+        monitor = 0;
+        follow = "none";
+        sticky_history = "yes";
+        history_length = 15;
+        show_indicators = "no";
+        line_height = 3;
+        separator_height = 2;
+        padding = 6;
+        horizontal_padding = 6;
+        separator_color = "frame";
+        startup_notification = "false";
+        dmenu = "${pkgs.rofi}/bin/rofi -p dunst";
+        browser = "${pkgs.firefox}/bin/firefox -new-tab";
+        icon_position = "off";
+        max_icon_size = 80;
+        #icon_path = /usr/share/icons/Paper/16x16/mimetypes/:/usr/share/icons/Paper/48x48/status/:/usr/share/icons/Paper/16x16/devices/:/usr/share/icons/Paper/48x48/notifications/:/usr/share/icons/Paper/48x48/emblems/;
+        frame_width = 3;
+        frame_color = "#8EC07C";
+      };
+      shortcuts = {
+        close = "ctrl+space";
+        close_all = "ctrl+shift+space";
+        history = "ctrl+grave";
+        context = "ctrl+shift+period";
+      };
+      urgency_low = {
+        frame_color = "#3B7C87";
+        foreground = "#3B7C87";
+        background = "#191311";
+        timeout = 4;
+      };
+      urgency_normal = {
+        frame_color = "#5B8234";
+        foreground = "#5B8234";
+        background = "#191311";
+        timeout = 6;
+      };
+      urgency_critical = {
+        frame_color = "#B7472A";
+        foreground = "#B7472A";
+        background = "#191311";
+        timeout = 8;
       };
     };
   };
@@ -131,7 +198,7 @@
       menu = "${pkgs.rofi}/bin/rofi -show run";
       startup = [
         { command = "pkill picom; ${pkgs.picom}/bin/picom --backend xrender"; notification = false; }
-        { command = "pkill dunst; ${pkgs.dunst}/bin/dunst"; notification = false; }
+        # { command = "pkill dunst; ${pkgs.dunst}/bin/dunst"; notification = false; }
       ];
       keybindings = let mod = config.modifier; in {
         "${mod}+d" = "exec ${config.menu}";
