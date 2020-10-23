cff92bd3755996278cda3ba6b0ad7f89a1b25f10
Tristan Gosselin-Hane <starcraft66@gmail.com>
2020-10-22 23:03:30 -0400

    Migrate to starship 0.45

--- home-manager/programs/starship.nix
+++ home-manager/programs/starship.nix
@@ -1,34 +1,14 @@
 { config, lib, pkgs, ... }:
 
 let
-  unstableTarball = fetchTarball "https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz";
+  unstableTarball = fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
   unstable = import unstableTarball {};
   inherit (pkgs.stdenv.targetPlatform) isDarwin isLinux;
 in
 {
   programs.starship = {
-    # Use starship master so that I can get the new formatting syntax
-    # before starship 0.45 is officially released
-
-    package = unstable.starship.overrideAttrs (oldAttrs: rec {
-      version = "0.44.999999999999999999";
-      src = pkgs.fetchFromGitHub {
-        owner = "starship";
-        repo = oldAttrs.pname;
-        rev = "2996220568d5dcc437b09dcfa2654a90c3ed9809";
-        sha256 = "0qmpbl7kzv7ighh8vw71mmxzcfay6cls6ny3qc4mx87l2x3ywssb";
-      };
-      cargoDeps = oldAttrs.cargoDeps.overrideAttrs (lib.const {
-        name = "${oldAttrs.pname}-${version}-vendor.tar.gz";
-        inherit src;
-        outputHash = "05cwvpagbqcwbnfjyb73dywvdhjl4jbj9vh1w6k977x2yh9vz7d8";
-      });
-      # Tests don't work on this dev version for some reason
-      doCheck = false;
-    });
-
-
     enable = true;
+    package = unstable.starship;
     enableBashIntegration = true;
     enableZshIntegration = true;
     enableFishIntegration = false;
@@ -37,47 +17,6 @@ in
       # This line replaces add_newline = false
       add_newline = false;
       format = "$all";
-      prompt_order = [
-        "username"
-        "hostname"
-        "git_branch"
-        "git_commit"
-        "git_state"
-        "git_status"
-        "hg_branch"
-        "docker_context"
-        "package"
-        "dotnet"
-        "elixir"
-        "elm"
-        "erlang"
-        "golang"
-        "haskell"
-        "java"
-        "julia"
-        "nodejs"
-        "ocaml"
-        "php"
-        "purescript"
-        "python"
-        "ruby"
-        "rust"
-        "terraform"
-        "zig"
-        "nix_shell"
-        "conda"
-        "memory_usage"
-        "env_var"
-        "crystal"
-        "cmd_duration"
-        "custom"
-        "line_break"
-        "jobs"
-        "battery"
-        "directory"
-        "time"
-        "character"
-      ];
 
       character = {
         symbol = "$";
@@ -90,7 +29,6 @@ in
 
       directory = {
         disabled = false;
-        prefix = "";
         truncation_length = 999;
         truncate_to_repo = false;
       };
@@ -115,7 +53,6 @@ in
       time = {
         disabled = true;
         format = "%H:%M";
-        prefix = "";
       };
 
       username = {
