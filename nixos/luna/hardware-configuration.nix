75bf7f45016db177835e7b11e0f06e006b57fe53
Tristan Gosselin-Hane <starcraft66@gmail.com>
2020-11-19 15:24:35 -0500

    Make PS4 controllers work

--- nixos/luna/hardware-configuration.nix
+++ nixos/luna/hardware-configuration.nix
@@ -45,4 +45,18 @@
     default-sample-rate = "44100";
     avoid-resampling = "true";
   };
+
+  # Make steam detect my DualShock 4 "Slim" controller (and others for reference)
+  services.udev.extraRules = ''
+    # DualShock 4 over USB hidraw
+    KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="05c4", MODE="0666"
+    # DualShock 4 wireless adapter over USB hidraw
+    KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="0ba0", MODE="0666"
+    # DualShock 4 Slim over USB hidraw
+    KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="09cc", MODE="0666"
+    # DualShock 4 over bluetooth hidraw
+    KERNEL=="hidraw*", KERNELS=="*054C:05C4*", MODE="0666"
+    # DualShock 4 Slim over bluetooth hidraw
+    KERNEL=="hidraw*", KERNELS=="*054C:09CC*", MODE="0666"
+  '';
 }
