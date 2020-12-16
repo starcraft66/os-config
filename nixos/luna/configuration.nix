a3c0093001298601181b70132bfc334196c46c4b
Tristan Gosselin-Hane <starcraft66@gmail.com>
2020-12-16 17:53:26 -0500

    Auto optimize nix store

--- nixos/luna/configuration.nix
+++ nixos/luna/configuration.nix
@@ -29,10 +29,13 @@ in
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Garbage Collection
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 30d";
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+    autoOptimiseStore = true;
   };
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
