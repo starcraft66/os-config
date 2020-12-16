fd027317befcb48134161071bcc48da1e71dfda6
Tristan Gosselin-Hane <starcraft66@gmail.com>
2020-12-16 17:54:25 -0500

    Enable browserpass and onedrive

--- nixos/luna/configuration.nix
+++ nixos/luna/configuration.nix
@@ -177,9 +177,13 @@ in
   # apcupsd battery shit
   services.apcupsd.enable = true;
 
+  # onedrive syncing
+  services.onedrive.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
+  programs.browserpass.enable = true;
   programs.gnupg.agent = { enable = true; enableSSHSupport = true; enableExtraSocket = true; };
   programs.wireshark = { enable = true; package = pkgs.wireshark; };
 
