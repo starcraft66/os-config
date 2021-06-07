a9c1f840a56418a8d4aaa1c1430461cde05f8ccf
Tristan Gosselin-Hane <starcraft66@gmail.com>
2021-06-07 02:10:46 -0400

    Enable zoxide

--- home-manager/programs/zoxide.nix
+++ home-manager/programs/zoxide.nix
@@ -1,10 +1,9 @@
 { config, lib, pkgs, ... }:
 
 {
-  # Disabled until 20.09 comes out
-  # programs.zoxide = {
-  #   enable = true;
-  #   enableZshIntegration = true;
-  #   enableFishIntegration = false;
-  # };
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+    enableFishIntegration = false;
+  };
 }
