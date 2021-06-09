19ceade62533ce4736b975d62745fe3f19998f2f
Tristan Gosselin-Hane <starcraft66@gmail.com>
2021-06-09 14:30:19 -0400

    helia: Remove duplicate module

--- hosts/helia/configuration.nix
+++ hosts/helia/configuration.nix
@@ -78,7 +78,6 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
   programs.wireshark = { enable = true; package = pkgs.wireshark; };
 
   services.pcscd.enable = true;
