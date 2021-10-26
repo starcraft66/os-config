1884325538d8f5489c5655eae109a2e4579c4125
Tristan Gosselin-Hane <starcraft66@gmail.com>
2021-10-26 02:47:47 -0400

    doom-emacs: don't load init.el twice

--- home-manager/programs/doom-emacs.nix
+++ home-manager/programs/doom-emacs.nix
@@ -4,24 +4,16 @@ let inherit (pkgs.stdenv.hostPlatform) isDarwin isLinux;
 in
 lib.mkMerge [
   {
-  # imports = [
-  #   inputs.nix-doom-emacs.hmModule
-  # ];
+    home.packages = with pkgs; [
+      nodePackages.pyright
+      yaml-language-server
+    ];
 
-  home.packages = with pkgs; [
-    nodePackages.pyright
-    yaml-language-server
-  ];
-
-  programs.doom-emacs = {
-    enable = true;
-    doomPrivateDir = ../doom.d;
-    emacsPackage = pkgs.emacsGit;
-  };
-
-  home.file.".emacs.d/init.el".text = ''
-      (load "default.el")
-  '';
+    programs.doom-emacs = {
+      enable = true;
+      doomPrivateDir = ../doom.d;
+      emacsPackage = pkgs.emacsGit;
+    };
   }
   (lib.mkIf isLinux {
     services.emacs = {
