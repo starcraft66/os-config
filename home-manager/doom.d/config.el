7bb826cc8828a2af635163a0bb6deffffb84b1ff
Tristan Gosselin-Hane <starcraft66@gmail.com>
2021-11-10 20:08:37 -0500

    doom-emacs: Don't specify font size
    Let the DPI wizards figure it out :/

--- home-manager/doom.d/config.el
+++ home-manager/doom.d/config.el
@@ -18,7 +18,7 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "MesloLGS Nerd Font Mono" :size 20))
+(setq doom-font (font-spec :family "MesloLGS Nerd Font Mono"))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
