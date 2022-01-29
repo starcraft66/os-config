f94a09b664ec023c6123a122b56346a3a8a2237f
Tristan Gosselin-Hane <starcraft66@gmail.com>
2022-01-29 16:51:29 -0500

    nix: nop out global flake registry

--- applications/nix.nix
+++ applications/nix.nix
@@ -11,6 +11,9 @@
       experimental-features = nix-command flakes
       keep-outputs = true
       keep-derivations = true
+
+      # nop out the global flake registry
+      flake-registry = ${builtins.toFile "flake-registry" (builtins.toJSON { version = 2; flakes = [ ]; })}
     '';
     # Pin nixpkgs for older Nix tools
     nixPath = [ "nixpkgs=${pkgs.path}" ];
