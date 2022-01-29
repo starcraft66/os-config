d3f45a0e32f6098daca158a805ecec18c07aee2a
Tristan Gosselin-Hane <starcraft66@gmail.com>
2022-01-29 18:11:44 -0500

    nix: refactor module to support NixOS and nix-darwin in tandem

--- caches/nix-community.nix
+++ caches/nix-community.nix
@@ -1,10 +1,25 @@
-{
-  nix.settings = {
-    binary-caches = [
-      "https://nix-community.cachix.org"
-    ];
-    binary-cache-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
+{lib, pkgs, ...}:
+
+let
+  inherit (pkgs.stdenv.hostPlatform) isDarwin isLinux;
+  caches = [
+    "https://nix-community.cachix.org"
+  ];
+  publicKeys = [
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  ];
+
+  nixLinux = {
+    binary-caches = caches;
+    binary-cache-public-keys = publicKeys;
   };
+  nixDarwin = {
+    binaryCaches = caches;
+    binaryCachePublicKeys = publicKeys;
+  };
+in
+{
+  nix = if isLinux then nixLinux
+    else if isDarwin then nixDarwin
+    else lib.throw "Unknown operating system!!!";
 }
