567c60eee8be3810f6aec326f8ac07a7d211bd76
Tristan Gosselin-Hane <starcraft66@gmail.com>
2022-04-25 02:31:23 -0400

    flake: refactor to allow patching nixpkgs
    * drop distinction between nixpkgs and nixos inputs
    * fix nix 2.3 flake upgrade procedure
    * inline nixosSystem to allow module patching

--- flake.nix
+++ flake.nix
@@ -1,8 +1,7 @@
 {
   inputs = {
     home-manager.url = "github:nix-community/home-manager";
-    nixos.url = "github:nixos/nixpkgs/nixos-unstable-small";
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
     nix-darwin.url = "github:lnl7/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     sops-nix.url = "github:Mic92/sops-nix";
@@ -15,63 +14,86 @@
     nix-doom-emacs.inputs.emacs-overlay.follows = "emacs-overlay";
     nixos-nvidia-vgpu.url = "github:danielfullmer/nixos-nvidia-vgpu";
   };
-  outputs = inputs@{ self, nixos, nix-darwin, nixpkgs, sops-nix, home-manager, nix-doom-emacs, nixos-nvidia-vgpu, ... }: let
+  outputs = inputs@{ self, nix-darwin, nixpkgs, sops-nix, home-manager, nix-doom-emacs, nixos-nvidia-vgpu, ... }: let
     inherit (nixpkgs) lib;
 
     platforms = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
     forAllPlatforms = f: lib.genAttrs platforms (platform: f platform);
 
-    nixpkgsFor = forAllPlatforms (platform: nixpkgs: import nixpkgs {
-      system = platform;
-      overlays = lib.flatten [
-        (lib.optional (platform == "x86_64-linux")
-        (self: super: {
-          # Compile Mixxx using a PortAudio build that supports JACK
-          # Overriding PortAudio globally causes an expensive rebuild I want to avoid
-          # until the change is merged upstream
-          # https://github.com/NixOS/nixpkgs/pull/157561
-          mixxx = super.mixxx.override {
-            portaudio = super.portaudio.overrideAttrs (oldPortaudioAttrs: {
-              buildInputs = oldPortaudioAttrs.buildInputs ++ [ super.jack2 ];
-            });
-          };
-        }))
-        inputs.emacs-overlay.overlay
-        # Apple Silicon backport overlay:
-        # In other words, x86 packages to install instead of
-        # arm packages which don't build yet for any reason
-        (lib.optional (platform == "aarch64-darwin")
-        # Apple Silicon backport overlay:
-        # In other words, x86 packages to install instead of
-        # arm packages which don't build yet for any reason
-        (self: super: {
-          starship = super.starship.overrideAttrs (oldAttrs: rec {
-            pname = "starship";
+    # inlined LOL
+    nixosSystem' = args:
+      import "${nixpkgs}/nixos/lib/eval-config.nix" (args // {
+        modules = args.modules ++ [ {
+          system.nixos.versionSuffix = ".${lib.substring 0 8 (nixpkgs.lastModifiedDate or nixpkgs.lastModified or "19700101")}.${nixpkgs.shortRev or "dirty"}";
+          system.nixos.revision = lib.mkIf (nixpkgs ? rev) nixpkgs.rev;
+        } ];
+      });
 
-            version = "1.1.1";
+    patchedNixpkgs = forAllPlatforms (platform: let
+        originalNixpkgs = (import nixpkgs { system = platform; });
+      in originalNixpkgs.applyPatches {
+        name = "patched-nixpkgs";
+        src = nixpkgs;
+        patches = [
+          # (originalNixpkgs.fetchpatch { # https://github.com/NixOS/nixpkgs/pull/170108
+          #   url = "https://patch-diff.githubusercontent.com/raw/NixOS/nixpkgs/pull/170108.patch";
+          #   sha256 = "sha256-GoGcX/D1jY2DI6fHO56OsRC3jlJp6op/MjCzrfvh4HI=";
+          # })
+        ];
+      });
 
-            src = super.fetchFromGitHub {
-              owner = "starship";
-              repo = pname;
-              rev = "v${version}";
-              sha256 = "sha256-Rr0HCr/uJDsBQiKJIPdEL3WOaLgMY2Nq2JGOq4dEUxQ=";
+    nixpkgsFor = forAllPlatforms (platform:
+      import patchedNixpkgs.${platform} {
+        system = platform;
+        overlays = lib.flatten [
+          (lib.optional (platform == "x86_64-linux")
+          (self: super: {
+            # Compile Mixxx using a PortAudio build that supports JACK
+            # Overriding PortAudio globally causes an expensive rebuild I want to avoid
+            # until the change is merged upstream
+            # https://github.com/NixOS/nixpkgs/pull/157561
+            mixxx = super.mixxx.override {
+              portaudio = super.portaudio.overrideAttrs (oldPortaudioAttrs: {
+                buildInputs = oldPortaudioAttrs.buildInputs ++ [ super.jack2 ];
+              });
             };
+          }))
+          inputs.emacs-overlay.overlay
+          # Apple Silicon backport overlay:
+          # In other words, x86 packages to install instead of
+          # arm packages which don't build yet for any reason
+          (lib.optional (platform == "aarch64-darwin")
+          # Apple Silicon backport overlay:
+          # In other words, x86 packages to install instead of
+          # arm packages which don't build yet for any reason
+          (self: super: {
+            starship = super.starship.overrideAttrs (oldAttrs: rec {
+              pname = "starship";
 
-            cargoDeps = oldAttrs.cargoDeps.overrideAttrs (super.lib.const {
-              name = "${pname}-vendor.tar.gz";
-              inherit src;
-              outputHash = "sha256-Wt10f2T+uJx62z/rHmyylad6pvUsXDHX+QyhQjjB8eg=";
+              version = "1.1.1";
+
+              src = super.fetchFromGitHub {
+                owner = "starship";
+                repo = pname;
+                rev = "v${version}";
+                sha256 = "sha256-Rr0HCr/uJDsBQiKJIPdEL3WOaLgMY2Nq2JGOq4dEUxQ=";
+              };
+
+              cargoDeps = oldAttrs.cargoDeps.overrideAttrs (super.lib.const {
+                name = "${pname}-vendor.tar.gz";
+                inherit src;
+                outputHash = "sha256-Wt10f2T+uJx62z/rHmyylad6pvUsXDHX+QyhQjjB8eg=";
+              });
             });
-          });
-          # This is bad for libraries but okay for programs.
-          # See: https://github.com/LnL7/nix-darwin/issues/334#issuecomment-850857148
-          # For libs, I will use pkgsX86 defined below.
-          # inherit (nixpkgsX86darwin) kitty;
-        }))
-      ];
-      config.allowUnfree = true;
-    });
+            # This is bad for libraries but okay for programs.
+            # See: https://github.com/LnL7/nix-darwin/issues/334#issuecomment-850857148
+            # For libs, I will use pkgsX86 defined below.
+            # inherit (nixpkgsX86darwin) kitty;
+          }))
+        ];
+        config.allowUnfree = true;
+      });
 
     nixpkgsX86darwin = import nixpkgs {
       localSystem = "x86_64-darwin";
@@ -79,7 +101,7 @@
   in {
 
     devShell = forAllPlatforms (platform: let
-        pkgs = nixpkgsFor.${platform} inputs.nixpkgs;
+        pkgs = nixpkgsFor.${platform};
         sops = inputs.sops-nix.packages.${platform};
       in pkgs.mkShell {
         sopsPGPKeyDirs = [
@@ -94,18 +116,19 @@
           sops.sops-import-keys-hook
         ];
 
-        NIX_CONF_DIR = let
-          current = lib.optionalString (builtins.pathExists /etc/nix/nix.conf) (builtins.readFile /etc/nix/nix.conf);
-          nixConf = pkgs.writeTextDir "etc/nix.conf" ''
-            ${current}
-            experimental-features = nix-command flakes
-          '';
-        in "${nixConf}/etc";
+        NIX_CONF_DIR = (pkgs.writeTextDir "etc/nix.conf" ''
+          experimental-features = nix-command flakes
+
+          !include /etc/nix/nix.conf
+          # include /etc/nix/doesnt-exist-nix.conf
+        '') + "/etc";
       });
 
     nixosConfigurations = {
-      luna = nixos.lib.nixosSystem {
+      luna = let
         system = "x86_64-linux";
+      in nixosSystem' {
+        system = system;
         modules = [
           home-manager.nixosModules.home-manager
           {
@@ -118,11 +141,13 @@
           ./modules
           ./hosts/luna/configuration.nix
         ];
-        pkgs = nixpkgsFor.x86_64-linux nixos;
+        pkgs = nixpkgsFor.${system};
         specialArgs = { inherit inputs; };
       };
-      helia = nixos.lib.nixosSystem {
+      helia = let
         system = "x86_64-linux";
+      in nixosSystem' {
+        system = system;
         modules = [
           home-manager.nixosModules.home-manager
           {
@@ -133,7 +158,7 @@
           sops-nix.nixosModules.sops
           ./hosts/helia/configuration.nix
         ];
-        pkgs = nixpkgsFor.x86_64-linux nixos;
+        pkgs = nixpkgsFor.${system};
         specialArgs = { inherit inputs; };
       };
     };
@@ -149,7 +174,7 @@
           ./hosts/nightmaremoon/darwin-configuration.nix
         ];
         specialArgs = {
-          pkgs = nixpkgsFor.x86_64-darwin inputs.nixpkgs;
+          pkgs = nixpkgsFor.x86_64-darwin;
           inputs = inputs // { darwin = inputs.nix-darwin; };
         };
       };
@@ -166,7 +191,7 @@
         specialArgs = {
           # Was having trouble getting nix to serve me arm64 packages
           # so we are being explicit here :)
-          pkgs = nixpkgsFor.aarch64-darwin inputs.nixpkgs;
+          pkgs = nixpkgsFor.aarch64-darwin;
           # Rosetta 2 is installed, define a special pkgsX86 to install
           # packages that don't build on aarch64-darwin yet as a fallback
           pkgsX86 = nixpkgsX86darwin;
