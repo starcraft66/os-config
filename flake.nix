101b0f35bd168ba3717a95b7fcb0046cb7690ef2
Tristan Gosselin-Hane <starcraft66@gmail.com>
2022-09-13 00:45:25 -0400

    element-web: fix breakage

--- flake.nix
+++ flake.nix
@@ -34,10 +34,10 @@
         name = "patched-nixpkgs";
         src = nixpkgs;
         patches = [
-          # (originalNixpkgs.fetchpatch { # https://github.com/NixOS/nixpkgs/pull/170108
-          #   url = "https://patch-diff.githubusercontent.com/raw/NixOS/nixpkgs/pull/173859.patch";
-          #   sha256 = "sha256-1zNdGaizEH24HSNsPilC7YWWx35G+JBE0rKVcUgl4rc=";
-          # })
+          (originalNixpkgs.fetchpatch { # https://github.com/NixOS/nixpkgs/pull/170108
+            url = "https://patch-diff.githubusercontent.com/raw/NixOS/nixpkgs/pull/190950.patch";
+            sha256 = "sha256-nEAiTgtSd9+nR1bgpUhlARF+i5PxblLi8ti+nm0AVVg=";
+          })
         ];
       });
 
