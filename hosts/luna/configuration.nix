7d1b26f05825254155351587d7e8d9e766d3cdbd
Tristan Gosselin-Hane <starcraft66@gmail.com>
2022-11-12 23:41:40 -0500

    luna: linuxPackages_5_18 -> linuxPackages_5_19

--- hosts/luna/configuration.nix
+++ hosts/luna/configuration.nix
@@ -17,7 +17,7 @@
   boot.supportedFilesystems = [ "ntfs" ];
 
   # Use the latest linux kernel
-  boot.kernelPackages = pkgs.linuxPackages_5_18;
+  boot.kernelPackages = pkgs.linuxPackages_5_19;
 
   networking.hostName = "luna"; # Define your hostname.
 
