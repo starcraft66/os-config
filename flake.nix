8b311ac2591c1e2a968028a532d8c1c2c58a70b7
Tristan Gosselin-Hane <starcraft66@gmail.com>
2023-01-31 21:29:12 -0500

    flake: hold back x86 packages broken in unstable

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     home-manager.url = "github:nix-community/home-manager";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/22.11";
     nix-darwin.url = "github:lnl7/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     sops-nix.url = "github:Mic92/sops-nix";
@@ -12,7 +13,7 @@
     nix-doom-emacs.inputs.emacs-overlay.follows = "emacs-overlay";
     nixos-nvidia-vgpu.url = "github:danielfullmer/nixos-nvidia-vgpu";
   };
-  outputs = inputs@{ self, nix-darwin, nixpkgs, sops-nix, home-manager, nix-doom-emacs, nixos-nvidia-vgpu, ... }: let
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nixpkgs-stable, sops-nix, home-manager, nix-doom-emacs, nixos-nvidia-vgpu, ... }: let
     inherit (nixpkgs) lib;
 
     platforms = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
@@ -34,19 +35,25 @@
         name = "patched-nixpkgs";
         src = nixpkgs;
         patches = [
-#          (originalNixpkgs.fetchpatch { # https://github.com/NixOS/nixpkgs/pull/170108
-#            url = "https://patch-diff.githubusercontent.com/raw/NixOS/nixpkgs/pull/190950.patch";
-#            sha256 = "sha256-nEAiTgtSd9+nR1bgpUhlARF+i5PxblLi8ti+nm0AVVg=";
-#          })
+#         (originalNixpkgs.fetchpatch { # https://github.com/NixOS/nixpkgs/pull/211469
+#           url = "https://patch-diff.githubusercontent.com/raw/NixOS/nixpkgs/pull/211469.patch";
+#           sha256 = "sha256-hzLTAMKnTMMrj9I3liS6AkCtat2purWE6CdlLcVt5jY=";
+#         })
         ];
       });
 
-    nixpkgsFor = forAllPlatforms (platform:
-      import patchedNixpkgs.${platform} {
+
+      nixpkgsFor = forAllPlatforms (platform: let
+        stableNixpkgs = (import nixpkgs-stable { system = platform; });
+      in (import patchedNixpkgs.${platform} {
         system = platform;
         overlays = lib.flatten [
           (lib.optional (platform == "x86_64-linux")
           (self: super: {
+            # Use packages from stable because they are broken on unstable
+            inherit (stableNixpkgs.pkgs) gmic-qt helvum;
+            python39Packages = super.python39Packages // { inherit (stableNixpkgs.pkgs.python39Packages) h2; };
+
             # Compile Mixxx using a PortAudio build that supports JACK
             # Overriding PortAudio globally causes an expensive rebuild I want to avoid
             # until the change is merged upstream
@@ -73,7 +80,7 @@
           }))
         ];
         config.allowUnfree = true;
-      });
+      }));
 
     nixpkgsX86darwin = import nixpkgs {
       localSystem = "x86_64-darwin";
