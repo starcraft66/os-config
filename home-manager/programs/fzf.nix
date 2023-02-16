7f9b2e5f729a249af928d51a98704ffc7e8f74eb
Tristan Gosselin-Hane <starcraft66@gmail.com>
2023-02-16 02:48:17 -0500

    fzf: use kl instead of jk for up down

--- home-manager/programs/fzf.nix
+++ home-manager/programs/fzf.nix
@@ -5,7 +5,7 @@
     enable = true;
     enableZshIntegration = true;
     defaultCommand = ''${pkgs.fd}/bin/fd --follow --type f --exclude="'.git'" .'';
-    defaultOptions = [ "--exact" "--cycle" "--layout=reverse" ];
+    defaultOptions = [ "--exact" "--cycle" "--layout=reverse" "--bind=ctrl-k:down,ctrl-l:up" ];
     enableFishIntegration = false;
   };
 }
