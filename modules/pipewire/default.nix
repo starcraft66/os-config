d2a648d549fe04da63069a02e4391c3b1aec4002
Tristan Gosselin-Hane <starcraft66@gmail.com>
2023-05-04 21:41:22 -0400

    pipewire: comment out config since its support is removed

--- modules/pipewire/default.nix
+++ modules/pipewire/default.nix
@@ -30,50 +30,50 @@ in
         pulse.enable = true;
         jack.enable = true;
 
-        config.pipewire = {
-          "context.properties" = {
-            #"link.max-buffers" = 64;
-            "link.max-buffers" = 16; # version < 3 clients can't handle more than this
-            "log.level" = 2; # https://docs.pipewire.org/page_daemon.html
-            #"default.clock.rate" = 48000;
-            #"default.clock.quantum" = 1024;
-            #"default.clock.min-quantum" = 32;
-            #"default.clock.max-quantum" = 8192;
+        # config.pipewire = {
+        #   "context.properties" = {
+        #     #"link.max-buffers" = 64;
+        #     "link.max-buffers" = 16; # version < 3 clients can't handle more than this
+        #     "log.level" = 2; # https://docs.pipewire.org/page_daemon.html
+        #     #"default.clock.rate" = 48000;
+        #     #"default.clock.quantum" = 1024;
+        #     #"default.clock.min-quantum" = 32;
+        #     #"default.clock.max-quantum" = 8192;
 
-          };
-          "context.objects" = [
-            {
-              # A default dummy driver. This handles nodes marked with the "node.always-driver"
-              # properyty when no other driver is currently active. JACK clients need this.
-              factory = "spa-node-factory";
-              args = {
-                "factory.name"     = "support.node.driver";
-                "node.name"        = "Dummy-Driver";
-                "priority.driver"  = 8000;
-              };
-            }
-            {
-              factory = "adapter";
-              args = {
-                "factory.name"     = "support.null-audio-sink";
-                "node.name"        = "Microphone-Proxy";
-                "node.description" = "Microphone";
-                "media.class"      = "Audio/Source/Virtual";
-                "audio.position"   = "MONO";
-              };
-            }
-            {
-              factory = "adapter";
-              args = {
-                "factory.name"     = "support.null-audio-sink";
-                "node.name"        = "Main-Output-Proxy";
-                "node.description" = "Main Output";
-                "media.class"      = "Audio/Sink";
-                "audio.position"   = "FL,FR";
-              };
-            }
-          ];
-        };
+        #   };
+        #   "context.objects" = [
+        #     {
+        #       # A default dummy driver. This handles nodes marked with the "node.always-driver"
+        #       # properyty when no other driver is currently active. JACK clients need this.
+        #       factory = "spa-node-factory";
+        #       args = {
+        #         "factory.name"     = "support.node.driver";
+        #         "node.name"        = "Dummy-Driver";
+        #         "priority.driver"  = 8000;
+        #       };
+        #     }
+        #     {
+        #       factory = "adapter";
+        #       args = {
+        #         "factory.name"     = "support.null-audio-sink";
+        #         "node.name"        = "Microphone-Proxy";
+        #         "node.description" = "Microphone";
+        #         "media.class"      = "Audio/Source/Virtual";
+        #         "audio.position"   = "MONO";
+        #       };
+        #     }
+        #     {
+        #       factory = "adapter";
+        #       args = {
+        #         "factory.name"     = "support.null-audio-sink";
+        #         "node.name"        = "Main-Output-Proxy";
+        #         "node.description" = "Main Output";
+        #         "media.class"      = "Audio/Sink";
+        #         "audio.position"   = "FL,FR";
+        #       };
+        #     }
+        #   ];
+        # };
       };
     })
     (mkIf cfg.lowlatency.enable {
