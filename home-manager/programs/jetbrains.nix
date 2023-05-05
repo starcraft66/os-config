edaad3c08de832780839d9bca2e3da023c327ef8
Tristan Gosselin-Hane <starcraft66@gmail.com>
2023-05-04 21:42:54 -0400

    jetbrains: add jetbrains ide + plugins via nix-ld

---
+++ home-manager/programs/jetbrains.nix
@@ -0,0 +1,28 @@
+{ config, osConfig, lib, pkgs, ... }:
+
+let inherit (pkgs.stdenv.hostPlatform) isDarwin isLinux;
+in
+lib.mkMerge [
+  (lib.mkIf isLinux (let
+    # GitHub Copilot
+    # nix-ld is required to run the copilot plugin
+    ides = let
+      NIX_LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [
+        stdenv.cc.cc
+      ];
+      withNixLd = (pkg: executable:
+        pkgs.writeShellScriptBin executable ''
+          export NIX_LD_LIBRARY_PATH=${NIX_LD_LIBRARY_PATH}
+          export NIX_LD=$(cat ${pkgs.stdenv.cc}/nix-support/dynamic-linker)
+          export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
+          exec ${pkg}/bin/${executable} "$@"
+        '');
+    in with pkgs; [
+      (withNixLd jetbrains.idea-ultimate "idea-ultimate")
+      (withNixLd jetbrains.clion "clion")
+      (withNixLd android-studio "android-studio")
+    ];
+  in {
+    home.packages = ides;
+  }))
+]
