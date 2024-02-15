bbe2d460a536d47d43c3f684df47ea61bdda62ab
Tristan Gosselin-Hane <starcraft66@gmail.com>
2024-02-14 21:44:59 -0500

    obs: add useful plugins

--- home-manager/programs/obs-studio.nix
+++ home-manager/programs/obs-studio.nix
@@ -4,6 +4,13 @@ let inherit (pkgs.stdenv.hostPlatform) isDarwin isLinux;
 in (lib.mkIf isLinux (with lib; {
   programs.obs-studio = {
     enable = true;
-    plugins = with pkgs.obs-studio-plugins; [ obs-move-transition ];
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-vaapi
+      obs-vkcapture
+      obs-nvfbc
+      obs-move-transition
+      obs-pipewire-audio-capture
+    ];
   };
 }))
