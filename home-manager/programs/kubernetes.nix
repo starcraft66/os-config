e5f6c1046fcd39f008e406a2f7f2b38c373f3f1a
Tristan Gosselin-Hane <starcraft66@gmail.com>
2024-10-22 14:06:12 +0000

    k8s: add some utilities

--- home-manager/programs/kubernetes.nix
+++ home-manager/programs/kubernetes.nix
@@ -4,6 +4,7 @@
   home.packages = with pkgs; [
     kubectl kubie kubetail k9s kubeconform kubeswitch
     kubespy kubernetes-helm kube-capacity kubent
+    ripgrep terraform opentofu
   ];
 
   programs.zsh.initExtra = ''
