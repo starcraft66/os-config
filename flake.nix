ca12431a8c523e8cac681279fd295a8a26a64ce5
Tristan Gosselin-Hane <starcraft66@gmail.com>
2024-12-07 13:17:53 -0500

    flake: advance nixpkgs-stable input

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     home-manager.url = "github:nix-community/home-manager";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/23.11";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/24.11-pre";
     nixos-wsl.url = "github:nix-community/NixOS-WSL";
     nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
     nix-darwin.url = "github:lnl7/nix-darwin/master";
