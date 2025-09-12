3bfa4d06281ec4e9e6b647272a9de0a1b3768ed6
Tristan Gosselin-Hane <starcraft66@gmail.com>
2025-09-12 20:05:07 +0200

    cloud: install colima on mac

--- home-manager/programs/cloud.nix
+++ home-manager/programs/cloud.nix
@@ -1,5 +1,7 @@
 { config, pkgs, lib, ... }:
 
+let inherit (pkgs.stdenv.hostPlatform) isDarwin isLinux;
+in
 {
-  home.packages = with pkgs; [ azure-cli kubelogin opentofu powershell openssl yq vault ];
+  home.packages = with pkgs; [ azure-cli kubelogin opentofu powershell openssl yq vault ] ++ lib.optionals isDarwin [ colima ];
 }
