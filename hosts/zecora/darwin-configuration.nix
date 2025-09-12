cad2b2a6e177f525222e1aaa7c61a631e49b3630
Tristan Gosselin-Hane <starcraft66@gmail.com>
2025-09-12 14:22:10 +0200

    Update nix-darwin and use determinate nix

--- hosts/zecora/darwin-configuration.nix
+++ hosts/zecora/darwin-configuration.nix
@@ -55,6 +55,9 @@
     user = "tristan";
   };
 
+  # Using Determinate Nix
+  nix.enable = false;
+
   homebrew = {
     enable = true;
     brewPrefix = "/opt/homebrew/bin";
@@ -97,54 +100,8 @@
     };
   };
 
-  # Use a custom configuration.nid location.
-  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
-  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
-
-  # Auto upgrade nix package and the daemon service.
-  nix = {
-    gc.automatic = true;
-    distributedBuilds = true;
-    buildMachines = [
-      {
-        systems = [ "x86_64-linux" ];
-        supportedFeatures = [ "kvm" "big-parallel" ];
-        sshUser = "root";
-        maxJobs = 32;
-        hostName = "lava.tdude.co";
-        sshKey = "/Users/tristan/.ssh/id_nixstore_lava";
-        # publicHostKey = "AAAAC3NzaC1lZDI1NTE5AAAAIBdrtUDqfsbYGx6e2K7BfRiL8WfF3tycSwFj3lVfJFyL";
-      }
-#      {
-#        systems = [ "x86_64-linux" ];
-#        supportedFeatures = [ "kvm" "big-parallel" ];
-#        sshUser = "tristan";
-#        maxJobs = 24;
-#        hostName = "luna.local";
-#        sshKey = "/Users/tristan/.ssh/id_nixstore_luna";
-#        # publicHostKey = "AAAAC3NzaC1lZDI1NTE5AAAAIBdrtUDqfsbYGx6e2K7BfRiL8WfF3tycSwFj3lVfJFyL";
-#      }
-#      {
-#        systems = [ "x86_64-linux" ];
-#        supportedFeatures = [ "big-parallel" ];
-#        sshUser = "root";
-#        maxJobs = 8;
-#        hostName = "172.16.2.6";
-#        sshKey = "/Users/tristan/.ssh/id_nixstore_builder";
-#        # publicHostKey = "H+DeIUeuXgqoDI+XcNL43mBheZGSIBRHrPz/mrIIQqw";
-#      }
-    ];
-    settings = {
-      # package = pkgs.nix;
-      trusted-users = [ "tristan" "@admin" ];
-      max-jobs = lib.mkDefault 8;
-
-      sandbox = false;
-      extra-sandbox-paths = [ "/System/Library/Frameworks" "/System/Library/PrivateFrameworks" "/usr/lib" "/private/tmp" "/private/var/tmp" "/usr/bin/env" ];
-    };
-  };
-
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 5;
+  system.primaryUser = "tristan";
 }
