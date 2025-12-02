56da384fc1fecad477e030591baa38465ee2b4a2
Tristan Gosselin-Hane <starcraft66@gmail.com>
2025-12-02 12:38:12 -0500

    home-manager: remove obsolete darwin stuff

--- home-manager/programs/darwin.nix
+++ home-manager/programs/darwin.nix
@@ -4,13 +4,4 @@ let
   inherit (pkgs.stdenv.hostPlatform) isDarwin isLinux;
 in
 (lib.mkIf isDarwin {
-  # Symlink Home Manager apps to ~/Applications/Home\ Manager\ Apps
-  # for Raycast (spotlight alternative)
-  home.file."Applications/Home Manager Apps".source = let
-    apps = pkgs.buildEnv {
-      name = "home-manager-applications";
-      paths = config.home.packages;
-      pathsToLink = "/Applications";
-    };
-  in "${apps}/Applications";
 })
