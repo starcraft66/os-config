36803d8062356d8641216b46845f9e3adc3a12d8
Tristan Gosselin-Hane <starcraft66@gmail.com>
2025-12-02 23:00:51 -0500

    sway: remove what's left of you

--- hosts/luna/configuration.nix
+++ hosts/luna/configuration.nix
@@ -439,12 +439,6 @@
     };
   };
 
-  programs.sway = {
-    enable = true;
-    extraOptions = [ " --unsupported-gpu " ];
-    wrapperFeatures.gtk = true;
-  };
-
   services.xserver.windowManager.i3 = {
     enable = true;
     package = pkgs.i3;
