95bb42aff304cc4c20f888bb2a7b1263c27e6fab
Tristan Gosselin-Hane <starcraft66@gmail.com>
2025-12-02 22:59:47 -0500

    nushell: remove hardcoded profile path

--- home-manager/programs/nushell.nix
+++ home-manager/programs/nushell.nix
@@ -1,11 +1,11 @@
-{ inputs, pkgs, osConfig, ... }:
+{ inputs, pkgs, osConfig, config, ... }:
 {
   programs.nushell = {
     enable = true;
     extraConfig = ''
       use ${inputs.bash-env-nushell.packages.${pkgs.stdenv.system}.default}/bash-env.nu
       bash-env ${osConfig.system.build.setEnvironment} | load-env
-      bash-env /etc/profiles/per-user/tristan.g-hane/etc/profile.d/hm-session-vars.sh | load-env
+      bash-env ${config.home.profileDirectory}/etc/profile.d/hm-session-vars.sh | load-env
       mkdir ($nu.data-dir | path join "vendor/autoload")
       tv init nu | save -f ($nu.data-dir | path join "vendor/autoload/tv.nu")
 
