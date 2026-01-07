d697420ecb84f149622fec2c245c5284ce9267f4
Tristan Gosselin-Hane <starcraft66@gmail.com>
2026-01-07 10:28:54 -0500

    cloud: install azure cli extensions and just

--- home-manager/programs/cloud.nix
+++ home-manager/programs/cloud.nix
@@ -1,10 +1,15 @@
 { config, pkgs, lib, ... }:
 
-let inherit (pkgs.stdenv.hostPlatform) isDarwin isLinux;
+let
+  inherit (pkgs.stdenv.hostPlatform) isDarwin isLinux;
+  # Install azure cli extensions
+  azure-cli = pkgs.azure-cli.withExtensions (with pkgs.azure-cli-extensions; [
+    aks-preview
+  ]);
 in
 {
-  home.packages = with pkgs; [
-    azure-cli
+  home.packages = (with pkgs; [
+    just
     kubelogin
     kubelogin-oidc
     opentofu
@@ -14,5 +19,7 @@ in
     vault
   ] ++ lib.optionals isDarwin [
     colima
+  ]) ++ [
+    azure-cli
   ];
 }
