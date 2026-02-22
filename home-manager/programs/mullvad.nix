0e74b74d236e4e63579ac71a7c902f647a1fbb0d
Tristan Gosselin-Hane <starcraft66@gmail.com>
2026-02-21 23:50:07 -0500

    mullvad: use hm module

--- home-manager/programs/mullvad.nix
+++ home-manager/programs/mullvad.nix
@@ -4,17 +4,5 @@ let
   inherit (pkgs.stdenv.hostPlatform) isDarwin isLinux;
 in
 (lib.mkIf isLinux {
-  systemd.user.services.mullvad-gui = {
-    Unit = {
-      Description = "Mullvad VPN GUI";
-      PartOf = [ "graphical-session.target" ];
-      After = [ "graphical-session-pre.target" ];
-    };
-    Service = {
-      Type = "simple";
-      ExecStart = "${pkgs.mullvad-vpn}/bin/mullvad-gui";
-      Restart = "on-abort";
-    };
-    Install.WantedBy = [ "graphical-session.target" ];
-  };
+  programs.mullvad-vpn.enable = true;
 })
