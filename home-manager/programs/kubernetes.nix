f31c97f3e74481bb105ff76812677a73e8904b69
Tristan Gosselin-Hane <starcraft66@gmail.com>
2026-03-05 11:53:47 -0500

    kubernetes: install helm plugins

--- home-manager/programs/kubernetes.nix
+++ home-manager/programs/kubernetes.nix
@@ -3,9 +3,16 @@
 {
   home.packages = with pkgs; [
     kubectl kubie kubetail k9s kubeconform kubeswitch
-    kubespy kubernetes-helm kube-capacity kubent
+    kubespy kube-capacity kubent
     ripgrep kubectx jq
     kind kubebuilder
+    (wrapHelm kubernetes-helm {
+      plugins = with kubernetes-helmPlugins; [
+        helm-secrets
+        helm-diff
+        helm-git
+      ];
+    })
   ];
 
   programs.zsh.initContent = lib.mkOrder 2000 ''
